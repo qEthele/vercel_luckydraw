39166eb9b5bcd0d19a615ff154ffb50f76308130
Thawatchai <yua.mch@gmail.com>
2023-02-27 13:05:30 +0700

    change server api

--- src/componenst/Api.jsx
+++ src/componenst/Api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiUrl = "https://api-honda-new.vercel.app";
+const apiUrl = "https://api-honda.onrender.com";
 // const apiUrl = "http://localhost:4000";
 
 export const Api = axios.create({ baseURL: apiUrl });
